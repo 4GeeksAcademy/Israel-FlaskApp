27372cd9d14672da9f0f545ae06d87312ca8470d
Israel Dellinger <51415637+idellinger@users.noreply.github.com>
2024-01-21 19:32:39 +0000

    Seth's fix for Errno

--- Israel/app.py
+++ Israel/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, render_template
 from pickle import load
+import os
 
 app = Flask(__name__)
+
+if os.path.isfile("/workspaces/Flaskandrender/src/decision_tree_classifier_default_4.sav"):
+    modelDir = "/workspaces/Flaskandrender/src/decision_tree_classifier_default_4.sav"
+else:
+    modelDir = "./decision_tree_classifier_default_4.sav"
+model = load(open(modelDir, "rb"))
 model = load(open("/workspaces/Israel-FlaskApp/Israel/decision_tree_classifier_default_42.sav", "rb"))
+
 class_dict = {
     "0": "Iris setosa",
     "1": "Iris versicolor",
